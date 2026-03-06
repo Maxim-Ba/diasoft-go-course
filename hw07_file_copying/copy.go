63226ec3849e947ffc2e2ee2a18668cf14e06cb8
Maxim Balashov <79164211428@yandex.ru>
2026-03-06 23:54:48 +0300

    test.sh has passed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,55 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fr, err := os.OpenFile(fromPath, os.O_RDONLY, 0666)
+	if err != nil {
+		return err
+	}
+	defer fr.Close()
+	// TODO fileInfo.Mode().IsRegular()
+	err = checkOffset(offset, fr)
+	limit, err = checkAndSetLimit(limit, fr)
+	if err != nil {
+		return err
+	}
+	fw, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer fw.Close()
+
+	_, err = fr.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("error Seek: %w", err)
+	}
+	limitedReader := io.LimitReader(fr, limit)
+
+	_, err = io.Copy(fw, limitedReader)
+	if err != nil {
+		return fmt.Errorf("error on copy: %w", err)
+	}
 	return nil
+}
+
+func checkOffset(offset int64, f *os.File) error {
+	fileInfo, err := f.Stat()
+	if err != nil {
+		return err
+	}
+
+	if offset > fileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	return nil
+}
+
+func checkAndSetLimit(limit int64, f *os.File) (int64, error) {
+	fileInfo, err := f.Stat()
+	if err != nil {
+		return limit, err
+	}
+	if limit == 0 {
+		limit = fileInfo.Size()
+	}
+	return limit, nil
 }
