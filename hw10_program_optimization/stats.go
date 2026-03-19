1cf19c6ee412b91c78ae6800f8548437c108f2e9
Maxim Balashov <79164211428@yandex.ru>
2026-03-19 08:01:28 +0300

    Add easyjson and scaner

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,13 +1,15 @@
+//go:generate easyjson -all stats.go
 package hw10programoptimization
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
 	"io"
 	"regexp"
 	"strings"
 )
 
+//easyjson:json
 type User struct {
 	ID       int
 	Name     string
@@ -21,42 +23,29 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
+	scanner := bufio.NewScanner(r)
+	return countDomains(scanner, domain)
 }
 
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
+func countDomains(scanner *bufio.Scanner, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 	// 1 компиляция regexp
 	re, err := regexp.Compile(`\.` + domain)
 	if err != nil {
 		return nil, err
 	}
-	for _, user := range u {
-		matched := re.MatchString(user.Email)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		if len(line) == 0 {
+			continue
+		}
 
+		var user User
+		// easyjson вместо encoding/json
+		if err := user.UnmarshalJSON(line); err != nil {
+			return nil, fmt.Errorf("unmarshal error: %w", err)
+		}
+		matched := re.MatchString(user.Email)
 		if matched {
 			domainPart := strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])
 			result[domainPart]++ // Вместо двух вызовов
