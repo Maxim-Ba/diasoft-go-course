efdfc279f4b01e8c8ef582fec162e25bc44a3e5f
Maxim Balashov <79164211428@yandex.ru>
2026-04-04 07:32:11 +0300

    Add config reader

--- hw12_13_14_15_16_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_16_calendar/cmd/calendar/main.go
@@ -8,16 +8,16 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/fixme_my_friend/hw12_13_14_15_16_calendar/internal/app"
-	"github.com/fixme_my_friend/hw12_13_14_15_16_calendar/internal/logger"
-	internalhttp "github.com/fixme_my_friend/hw12_13_14_15_16_calendar/internal/server/http"
-	memorystorage "github.com/fixme_my_friend/hw12_13_14_15_16_calendar/internal/storage/memory"
+	"github.com/github.com/Maxim-Ba/hw12_13_14_15_calendar/internal/app"
+	"github.com/github.com/Maxim-Ba/hw12_13_14_15_calendar/internal/logger"
+	internalhttp "github.com/github.com/Maxim-Ba/hw12_13_14_15_calendar/internal/server/http"
+	memorystorage "github.com/github.com/Maxim-Ba/hw12_13_14_15_calendar/internal/storage/memory"
 )
 
 var configFile string
 
 func init() {
-	flag.StringVar(&configFile, "config", "/etc/calendar/config.toml", "Path to configuration file")
+	flag.StringVar(&configFile, "config", "configs/config.yaml", "Path to configuration file")
 }
 
 func main() {
@@ -28,7 +28,12 @@ func main() {
 		return
 	}
 
-	config := NewConfig()
+	config, err := NewConfig(configFile)
+	if err != nil {
+		logger.New("ERROR").Error("failed to load config: " + err.Error())
+		os.Exit(1)
+	}
+
 	logg := logger.New(config.Logger.Level)
 
 	storage := memorystorage.New()
